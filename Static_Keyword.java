b48ec7dd966a3d708bb203a2e48cccdf8922688a
Hari Pratap SN <haripratapsn2003@gmail.com>
2024-12-23 21:52:30 +0530

    Update Static_Keyword.java

--- Static_Keyword.java
+++ Static_Keyword.java
@@ -1,13 +1,12 @@
 class Father4{
     static char gender='M';
-
+    static int age=50;
     static void print(){
         System.out.println("This is a Parent Class");
     }
 }
 class Daughter4 extends Father4{
     char gender='F';
-
     static void print(){
         System.out.println("This is child class");
     }
@@ -19,8 +18,10 @@ class Static{
         Daughter4.print();
         System.out.println(ob.gender);
         System.out.println(Father.gender);
+        System.out.println(Father4.age);  //Here we are calling father static age
+        Daughter4.age=18;                 //Now we can override the static age directly without creating any object
+        System.out.println(Father4.age);  //Father age variable now stores the overridden age data
     }
-
 }
 
 
